51727e4899a2ff1fd6afde9e16d1e0ff60586a6e
vimark <stanislava1804@gmail.com>
2022-11-06 14:12:19 +0200

    page 722-01 without zooming photos

--- page.js
+++ page.js
@@ -1,1 +1,22 @@
-let sidebar = document.querySelector('.sidebar');
+const contacts = document.querySelector(".cont");
+const main = document.querySelector(".main").clientHeight;
+const cont_height = main * 2;
+
+contacts.addEventListener('click', () =>{
+    window.scrollBy({
+        top: cont_height,
+        behavior: 'smooth'
+      });
+});
+
+function myFunction() {
+    var copyText = 'https://lucky-melba-0e136c.netlify.app/722-01.html';
+    navigator.clipboard.writeText(copyText);
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Link are copied";
+}
+  
+function outFunc() {
+    var tooltip = document.getElementById("myTooltip");
+    tooltip.innerHTML = "Copy to clipboard";
+}
