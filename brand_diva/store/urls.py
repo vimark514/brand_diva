3bb02e5491301ed6998ce53ac79d77c7d4793126
vimark <stanislava1804@gmail.com>
2024-04-08 21:47:23 +0200

    adding a cart and like

--- brand_diva/store/urls.py
+++ brand_diva/store/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.store, name='store'),
     path('cart/', views.cart, name='cart'),
-    path('about-us/', views.about_us, name='about_us'),
+    path('about_us/', views.about_us, name='about_us'),
     path('main/', views.main, name='main'),
 ]
